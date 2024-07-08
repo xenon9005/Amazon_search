e88e3aee7b65d39c1e8af845a26f5af8675e6742
prakash.rai <prakash.rai@NOI-PG02ZD4Q.intelegencia.com>
2024-07-08 18:36:22 +0530

    added the property file and wait

--- Amazone/src/test/java/Amazone/Amazone/amazon.java
+++ Amazone/src/test/java/Amazone/Amazone/amazon.java
@@ -1,9 +1,11 @@
 package Amazone.Amazone;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
+import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
@@ -18,7 +20,19 @@ public class amazon {
 	static WebDriver driver;
 	 static WebElement Element;
 	 static String value="";
-
+	 static String Sample="";
+	 static WebDriverWait wait;
+	 static Properties properties = new Properties();
+	 
+	 static {
+	        try (InputStream input = new FileInputStream("PageData.properties")) {
+	            properties.load(input);
+	        } catch (IOException e) {
+	            System.out.println("Failed to load properties file: PageData.properties");
+	            e.printStackTrace();
+	        }
+	    }     
+	 
 	public static void main(String[] args) {
 		
 		browserOpen();
@@ -26,24 +40,37 @@ public class amazon {
 		
 	}
 	
+	public static String BigData(String variable) {
+		String data=properties.getProperty(variable);
+		return data;
+	}
+	
+	
 	public static void browserOpen() {
 		
 		ChromeOptions options = new ChromeOptions();
 		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 		
 		driver = new ChromeDriver(options);
-		driver.manage().window().maximize();		
+		driver.manage().window().maximize();	
+		wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 			
 		try {
-		driver.get("https://www.amazon.in/");
+			//url open
+			Sample =BigData("url");
+			driver.get(Sample);
 		
-		Element = driver.findElement(By.id("nav-logo-sprites"));
-		value=Element.getAttribute("aria-label");
+			//Logo print 			
+					
+			Sample =BigData("title");		
+			Element = wait.until((ExpectedConditions.visibilityOfElementLocated(By.id(Sample))));
+			Sample =BigData("t1");
+			value=Element.getAttribute(Sample);
 		
-		System.out.println(value+ " successfully open!");
+			System.out.println(value+ " successfully open!");
 		}
 		catch(Exception e) {
-			String G = driver.findElement(By.tagName("Body")).getText();
+			String G = wait.until((ExpectedConditions.visibilityOfElementLocated(By.tagName("Body")))).getText();
 			System.out.println(G + e.getMessage());
 		}
 	}
@@ -52,32 +79,37 @@ public class amazon {
 	public static void SearchProduct() {
 		
 		try {		
-		Element = driver.findElement(By.id("twotabsearchtextbox"));
+			Sample=BigData("searchbox");
+			Element = wait.until((ExpectedConditions.visibilityOfElementLocated(By.id(Sample))));
 		
-		//Enter the product you want to search
-		value= "iphone";
-		Element.sendKeys(value);
-		Element=driver.findElement(By.id("nav-search-submit-button"));
-		Element.submit();
+			//Enter the product you want to search
+			value= "iphone";
+			Element.sendKeys(value);
+			//click on button
+			Sample=BigData("button");
+			Element=wait.until((ExpectedConditions.visibilityOfElementLocated(By.id(Sample))));
+			Element.submit();
 		
-		List<WebElement> listRecords = driver.findElements(By.cssSelector("div.s-main-slot div.s-result-item h2 a span"));
+			//list all results 
+			Sample=BigData("listresult");
+			List<WebElement> listRecords = driver.findElements(By.cssSelector(Sample));
 		
-		for (WebElement result : listRecords) {
-            String title = result.getText();
-            System.out.println("Title: " + title);
+			for (WebElement result : listRecords) {
+				String title = result.getText();
+				System.out.println("Title: " + title);
             
-            if (title.toLowerCase().contains(value.toLowerCase())) {
-                System.out.println("Matching title found: " + title);
+				if (title.toLowerCase().contains(value.toLowerCase())) {
+					System.out.println("Matching title found: " + title);
                 
-                
-                result.findElement(By.xpath("./ancestor::a")).click();
-                Thread.sleep(5000);
-                break;
+					Sample=BigData("ancestor");
+					result.findElement(By.xpath(Sample)).click();
+					// Thread.sleep(5000);
+					break;
             	}
 			}
 		}
 		catch(Exception ex) {
-			String G = driver.findElement(By.tagName("Body")).getText();
+			String G = wait.until((ExpectedConditions.visibilityOfElementLocated(By.tagName("Body")))).getText();
 			System.out.println(G + ex.getMessage());
 		}
 		
@@ -86,4 +118,3 @@ public class amazon {
 		}	   
 
 }
-
